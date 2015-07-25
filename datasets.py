908121d34ea0d5d3977b16ca270307fb3d02fcbc
James Knighton <iamknighton@gmail.com>
2015-07-25 16:26:34 -0700

    Fix horrible bug.

--- datasets.py
+++ datasets.py
@@ -15,7 +15,7 @@ def load_chinese_sent():
     pairs = map(lambda s: (s, 'pos'), poss) + map(lambda s: (s, 'neg'), negs)
     random.shuffle(pairs)
     n = len(pairs)
-    train = zip(*pairs[:int(n * 0.9)])
+    train = zip(*pairs[:int(n * 0.8)])
     val = zip(*pairs[int(n * 0.8):int(n * 0.9)])
     test = zip(*pairs[int(n * 0.9):])
     return train, val, test
